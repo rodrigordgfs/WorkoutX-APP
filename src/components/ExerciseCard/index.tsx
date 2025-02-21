03e9673bf4f5b19028cd2bc93a7a7e67488003ef
Rodrigo V. Rodrigues <rodrigordgfs@gmail.com>
2025-02-21 15:13:42 -0300

    feature: Adjuste complete exercise button

--- src/components/ExerciseCard/index.tsx
+++ src/components/ExerciseCard/index.tsx
@@ -1,19 +1,5 @@
-import {
-  Exercise,
-  ExerciseSession,
-  useWorkout,
-} from "@/context/WorkoutContext";
-import {
-  Check,
-  CheckCheck,
-  LoaderCircle,
-  Repeat,
-  Timer,
-  Weight,
-} from "lucide-react";
-import axios from "axios";
-import { useState } from "react";
-import { toast } from "react-toastify";
+import { Exercise } from "@/context/WorkoutContext";
+import { Repeat, Timer, Weight } from "lucide-react";
 
 interface ExerciseCardProps {
   exercise: Exercise;
@@ -22,58 +8,6 @@ interface ExerciseCardProps {
 }
 
 const ExerciseCard = ({ exercise, isActive, onSelect }: ExerciseCardProps) => {
-  const { workoutSession, setWorkoutSession, workoutSessionCompleted, workoutSessionInProgress } =
-    useWorkout();
-
-  const [loadingCompleteExercise, setLoadingCompleteExercise] = useState(false);
-
-  const workoutSessionExercise = workoutSession?.exercises.find(
-    (ex) => ex.exercise.id === exercise.id
-  );
-
-  const handleCompleteExercise = () => {
-    setLoadingCompleteExercise(true);
-    axios
-      .patch(
-        `/workout/session/${workoutSession?.id}/exercise/${workoutSessionExercise?.id}/complete`,
-        {
-          completed: !workoutSessionExercise?.completed,
-          weight: exercise.weight,
-          repetitions: exercise.repetitions,
-          series: exercise.series,
-        },
-        {
-          baseURL: import.meta.env.VITE_API_BASE_URL,
-        }
-      )
-      .then(({ data }) => {
-        setWorkoutSession({
-          id: data.id,
-          startedAt: data.startedAt,
-          endedAt: data.endedAt,
-          exercises: data.exercises.map((exercise: ExerciseSession) => ({
-            id: exercise.id,
-            series: exercise.series,
-            repetitions: exercise.repetitions,
-            weight: exercise.weight,
-            restTime: exercise.restTime,
-            completed: exercise.completed,
-            exercise: {
-              id: exercise.exercise.id,
-              name: exercise.exercise.name,
-            },
-          })),
-        });
-      })
-      .catch((error) => {
-        console.error(error);
-        toast.error("Erro ao concluir treino");
-      })
-      .finally(() => {
-        setLoadingCompleteExercise(false);
-      });
-  };
-
   return (
     <div
       className={`p-4 rounded-lg mb-2 cursor-pointer transition-all ${
@@ -85,35 +19,6 @@ const ExerciseCard = ({ exercise, isActive, onSelect }: ExerciseCardProps) => {
     >
       <div className="flex flex-row items-center justify-between gap-2">
         <h3 className="text-lg font-semibold mb-2">{exercise.name}</h3>
-        <button
-          onClick={() => {
-            if (!workoutSessionCompleted()) {
-              handleCompleteExercise();
-            }
-          }}
-          disabled={loadingCompleteExercise || !workoutSessionInProgress()}
-          className={`flex items-center justify-center p-2 rounded-full bg-blue-500 text-white transition-all ${
-            loadingCompleteExercise || !workoutSessionInProgress() && "cursor-not-allowed"
-          } ${
-            workoutSessionExercise?.completed
-              ? "bg-green-500"
-              : isActive
-              ? "bg-white text-blue-500"
-              : "hover:bg-blue-400"
-          }`}
-        >
-          {loadingCompleteExercise ? (
-            <LoaderCircle
-              size={16}
-              className="animate-spin"
-              color={isActive ? "#2563EB" : "#FFF"}
-            />
-          ) : workoutSessionExercise?.completed ? (
-            <CheckCheck size={16} />
-          ) : (
-            <Check size={16} color={isActive ? "#2563EB" : "#FFF"} />
-          )}
-        </button>
       </div>
       <div className="flex gap-4 text-sm">
         <div className="flex items-center gap-1">
--- src/components/WorkoutDetails/index.tsx
+++ src/components/WorkoutDetails/index.tsx
@@ -1,105 +1,211 @@
-import { Exercise, useWorkout } from "@/context/WorkoutContext";
+import {
+  Exercise,
+  ExerciseSession,
+  useWorkout,
+} from "@/context/WorkoutContext";
 import { Modal } from "../Modal";
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 interface WordkoutDetailsProps {
-    exercise: Exercise | null
+  exercise: Exercise | null;
 }
 
 const WorkoutDetails = ({ exercise }: WordkoutDetailsProps) => {
-    const navigate = useNavigate();
-    const { deleteExercise, getWorkoutByExerciseId, isLastExerciseInWorkout, deleteWorkout } = useWorkout();
+  const navigate = useNavigate();
+  const {
+    deleteExercise,
+    getWorkoutByExerciseId,
+    isLastExerciseInWorkout,
+    deleteWorkout,
+    workoutSession,
+    workoutSessionCompleted,
+    workoutSessionInProgress,
+    setWorkoutSession,
+  } = useWorkout();
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isDeleting, setIsDeleting] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [loadingCompleteExercise, setLoadingCompleteExercise] = useState(false);
 
-    const getEmbedUrl = (url: string | "") => {
-        const videoIdMatch = url?.match(/(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/);
-        return videoIdMatch ? `https://www.youtube.com/embed/${videoIdMatch[1]}` : url;
-    };
+  const workoutSessionExercise = workoutSession?.exercises.find(
+    (ex) => ex.exercise.id === exercise?.id
+  );
 
-    const handleDeleteExercise = async () => {
-        setIsDeleting(true);
-
-        const workout = getWorkoutByExerciseId(exercise?.id ?? "");
-
-        if (isLastExerciseInWorkout(workout, exercise?.id ?? "")) {
-            axios.delete(`/workout/${workout?.id}`, {
-                baseURL: import.meta.env.VITE_API_BASE_URL,
-            }).then(() => {
-                deleteWorkout(workout?.id ?? "");
-                setIsDeleting(false);
-                setIsModalOpen(false);
-                navigate("/");
-            }).catch((error) => {
-                console.error(error);
-                setIsDeleting(false);
-            });
-        } else {
-            axios.delete(`/workout/exercise/${exercise?.id}`, {
-                baseURL: import.meta.env.VITE_API_BASE_URL,
-            }).then(() => {
-                deleteExercise(exercise?.id ?? "");
-                setIsDeleting(false);
-                setIsModalOpen(false);
-            }).catch((error) => {
-                console.error(error);
-                setIsDeleting(false);
-            });
-        }
+  const handleCompleteExercise = () => {
+    if (!workoutSessionInProgress()) {
+      toast.info("Treino ainda não foi iniciado ou já esta concluído");
+      return;
     }
+    if (workoutSessionCompleted()) {
+      toast.info("Treino já foi concluído");
+      return;
+    }
+    setLoadingCompleteExercise(true);
+    axios
+      .patch(
+        `/workout/session/${workoutSession?.id}/exercise/${workoutSessionExercise?.id}/complete`,
+        {
+          completed: !workoutSessionExercise?.completed,
+          weight: exercise?.weight,
+          repetitions: exercise?.repetitions,
+          series: exercise?.series,
+        },
+        {
+          baseURL: import.meta.env.VITE_API_BASE_URL,
+        }
+      )
+      .then(({ data }) => {
+        setWorkoutSession({
+          id: data.id,
+          startedAt: data.startedAt,
+          endedAt: data.endedAt,
+          exercises: data.exercises.map((exercise: ExerciseSession) => ({
+            id: exercise.id,
+            series: exercise.series,
+            repetitions: exercise.repetitions,
+            weight: exercise.weight,
+            restTime: exercise.restTime,
+            completed: exercise.completed,
+            exercise: {
+              id: exercise.exercise.id,
+              name: exercise.exercise.name,
+            },
+          })),
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error("Erro ao concluir treino");
+      })
+      .finally(() => {
+        setLoadingCompleteExercise(false);
+      });
+  };
 
-    return <div className="bg-white rounded-lg p-6 shadow-lg">
-        <div className="flex items-center justify-between gap-4 mb-2">
-            <h2 className="text-2xl font-bold mb-4 mt-2">{exercise?.name}</h2>
-            {/* <button onClick={() => setIsModalOpen(true)} className="text-white bg-red-500 hover:bg-red-600 transition-all rounded-md p-2">
-                <Trash size={24} />
-            </button> */}
-        </div>
+  const getEmbedUrl = (url: string | "") => {
+    const videoIdMatch = url?.match(
+      /(?:youtube\.com\/(?:[^/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?/ ]{11})/
+    );
+    return videoIdMatch
+      ? `https://www.youtube.com/embed/${videoIdMatch[1]}`
+      : url;
+  };
 
-        <div className="aspect-video w-full mb-6">
-            <iframe
-                className="w-full h-full rounded-lg"
-                src={getEmbedUrl(exercise?.videoUrl ?? "")}
-                title={exercise?.name}
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-            ></iframe>
-        </div>
+  const handleDeleteExercise = async () => {
+    setIsDeleting(true);
 
-        <div className="space-y-4">
-            <div>
-                <h3 className="text-lg font-semibold mb-2">Instruções:</h3>
-                <p className="text-gray-600">{exercise?.instructions}</p>
+    const workout = getWorkoutByExerciseId(exercise?.id ?? "");
+
+    if (isLastExerciseInWorkout(workout, exercise?.id ?? "")) {
+      axios
+        .delete(`/workout/${workout?.id}`, {
+          baseURL: import.meta.env.VITE_API_BASE_URL,
+        })
+        .then(() => {
+          deleteWorkout(workout?.id ?? "");
+          setIsDeleting(false);
+          setIsModalOpen(false);
+          navigate("/");
+        })
+        .catch((error) => {
+          console.error(error);
+          setIsDeleting(false);
+        });
+    } else {
+      axios
+        .delete(`/workout/exercise/${exercise?.id}`, {
+          baseURL: import.meta.env.VITE_API_BASE_URL,
+        })
+        .then(() => {
+          deleteExercise(exercise?.id ?? "");
+          setIsDeleting(false);
+          setIsModalOpen(false);
+        })
+        .catch((error) => {
+          console.error(error);
+          setIsDeleting(false);
+        });
+    }
+  };
+
+  return (
+    <div className="bg-white rounded-lg p-6 shadow-lg">
+      <div className="flex flex-col md:flex-row items-center justify-between mb-2">
+        <h2 className="text-2xl font-bold mb-4 mt-2">{exercise?.name}</h2>
+        <button
+          onClick={handleCompleteExercise}
+          disabled={loadingCompleteExercise}
+          className={`py-2 px-4 rounded-lg text-white transition-all mb-2 md:mb-0 w-full md:w-auto ${
+            loadingCompleteExercise && "cursor-not-allowed opacity-75"
+          } ${
+            workoutSessionExercise?.completed
+              ? "bg-green-500 hover:bg-green-600"
+              : "bg-blue-500 hover:bg-blue-600"
+          }`}
+        >
+          {workoutSessionExercise?.completed ? (
+            <div className="flex items-center justify-center gap-2">
+              Exercício Concluído
             </div>
-
-            <div className="flex flex-col sm:flex-row gap-4">
-                <div className="bg-blue-50 p-4 rounded-lg text-center flex-1">
-                    <h4 className="font-semibold">Séries</h4>
-                    <p className="text-2xl font-bold text-blue-600">{exercise?.series}</p>
-                </div>
-                <div className="bg-blue-50 p-4 rounded-lg text-center flex-1">
-                    <h4 className="font-semibold">Repetições</h4>
-                    <p className="text-2xl font-bold text-blue-600">{exercise?.repetitions}</p>
-                </div>
-                <div className="bg-blue-50 p-4 rounded-lg text-center flex-1">
-                    <h4 className="font-semibold">Descanso</h4>
-                    <p className="text-2xl font-bold text-blue-600">{exercise?.restTime}s</p>
-                </div>
+          ) : (
+            <div className="flex items-center justify-center gap-2">
+              Concluir Exercício
             </div>
+          )}
+        </button>
+      </div>
+
+      <div className="aspect-video w-full mb-6">
+        <iframe
+          className="w-full h-full rounded-lg"
+          src={getEmbedUrl(exercise?.videoUrl ?? "")}
+          title={exercise?.name}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <div className="space-y-4">
+        <div>
+          <h3 className="text-lg font-semibold mb-2">Instruções:</h3>
+          <p className="text-gray-600">{exercise?.instructions}</p>
         </div>
 
-        <Modal
-            isOpen={isModalOpen}
-            onClose={() => setIsModalOpen(false)}
-            onConfirm={handleDeleteExercise}
-            loading={isDeleting}
-            title="Excluir Exercício"
-            content="Tem certeza que deseja excluir esse exercício?"
-        />
+        <div className="flex flex-col sm:flex-row gap-4">
+          <div className="bg-blue-50 p-4 rounded-lg text-center flex-1">
+            <h4 className="font-semibold">Séries</h4>
+            <p className="text-2xl font-bold text-blue-600">
+              {exercise?.series}
+            </p>
+          </div>
+          <div className="bg-blue-50 p-4 rounded-lg text-center flex-1">
+            <h4 className="font-semibold">Repetições</h4>
+            <p className="text-2xl font-bold text-blue-600">
+              {exercise?.repetitions}
+            </p>
+          </div>
+          <div className="bg-blue-50 p-4 rounded-lg text-center flex-1">
+            <h4 className="font-semibold">Descanso</h4>
+            <p className="text-2xl font-bold text-blue-600">
+              {exercise?.restTime}s
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={handleDeleteExercise}
+        loading={isDeleting}
+        title="Excluir Exercício"
+        content="Tem certeza que deseja excluir esse exercício?"
+      />
     </div>
-}
+  );
+};
 
-export default WorkoutDetails
+export default WorkoutDetails;
